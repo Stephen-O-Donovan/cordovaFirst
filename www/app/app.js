c9a87cf08150ce86d0303905cd3eb9049948f889
Stephen O'Donovan <booklad@gmail.com>
2019-04-12 15:49:23 +0100

    First commit

---
+++ www/app/app.js
@@ -0,0 +1,41 @@
+// app.js - Business logic here
+
+    // Creating module with name cordovaAngularApp
+    var app = angular.module('cordovaAngularApp', []);
+
+    app.config(function($routeProvider){
+
+    });
+
+    // creating the controller and inject Angular's $scope
+    app.controller('mainController', function($scope) {
+
+        // create a message to display in our view
+        $scope.message = 'Excited to build hybrid app';
+
+        //Function to capture image
+        $scope.captureImage = function() {
+            $scope.message="Button clicked";
+        };
+
+        //Display way to change data
+        $scope.cScopeName = "This should be changeable";
+
+        $scope.quantity = 0;
+        $scope.cost = 0;
+    });
+
+    // create the controller for the home page
+    app.controller('home Controller', function($scope) {
+        $scope.message = 'This is the home page';
+    });
+
+    // create the controller for the login page
+    app.controller('loginController', function($scope) {
+        $scope.message = 'This is the login page';
+    });
+
+    // create the controller for the register page
+    app.controller('registerController', function($scope) {
+        $scope.message = 'This is the register page';
+    });
